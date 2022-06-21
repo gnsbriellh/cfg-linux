fc4e7f2581be5a76acc14c4c58cf7abfd150fc1d
Gabriel N. Silva <106001507+gnsbriellh@users.noreply.github.com>
2022-06-21 03:13:24 -0300

    changed to arch based distro

--- update.sh
+++ update.sh
@@ -1,96 +1,63 @@
 #!/bin/bash
 
-## Backup and Swap .bachrc file (New,Current and Root Users) and insert snippets file, for the terminal ##
-
-    BACKUP_FOLDER="/home/$USER/Documents/DotFilesBackup"
-    mkdir $BACKUP_FOLDER -p
-
-    cp /home/$USER/.bashrc $BACKUP_FOLDER/.bashrc.user-backup
-    cp $PWD/dotfiles/.bashrc-user /home/$USER/.bashrc
-    cp $PWD/dotfiles/snippets /home/$USER/.bash_aliases
-
-    sudo cp /root/.bashrc $BACKUP_FOLDER/.bashrc.root-backup
-    sudo cp $PWD/dotfiles/.bashrc-root /root/.bashrc
-    sudo cp $PWD/dotfiles/snippets /root/.bash_aliases
-
-    sudo cp /etc/skel/.bashrc $BACKUP_FOLDER/.bashrc.skel-backup
-    sudo cp $PWD/dotfiles/.bashrc-user /etc/skel/.bashrc
-    sudo cp $PWD/dotfiles/snippets /etc/skel/.bash_aliases
-
-## Download/Install External Apps ##
-
-    DOWNLOADS_DIR="$PWD/Downloads"
-    mkdir $DOWNLOADS_DIR -p ; cd $DOWNLOADS_DIR ;
-
-    wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-    sudo dpkg -i *.deb
-
-## Updating and Installing Softwares ##
-
-    sudo apt update -y ; sudo apt upgrade -y ;
-
-    sudo apt install git -y
-    sudo apt install virtualbox -y
-    sudo apt install piper -y
-    sudo apt install gparted -y
-    sudo apt install steam -y
-    sudo apt install code -y
-    sudo apt install discord -y
-    sudo apt install gnome-tweaks -y
-    sudo apt install thunderbird -y
-    sudo apt install python3 python3-venv python3-pip -y
-    #sudo apt install ubuntu-restricted-extras -y
-    sudo apt install snapd -y
-    sudo snap install spotify
-    sudo snap install telegram-desktop
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-    nvm install 16 ; nvm use 16
-
-    sudo apt update -y ; sudo apt upgrade -y ;
-
-## Open Extentions URL ##
-echo "press [ENTER] to continue"
-read Enter
-sensible-browser \
-https://chrome.google.com/webstore/detail/gnome-shell-integration/gphhapmejobijbbhgpjhcjognlahblep \
-https://extensions.gnome.org/extension/7/removable-drive-menu/ \
-https://extensions.gnome.org/extension/906/sound-output-device-chooser/ \
-https://extensions.gnome.org/extension/19/user-themes/
-
-
-#!/bin/bash
-
 ## Customize Terminal ##
 
+	BACKUP_FOLDER="/home/$USER/Documents/DotFilesBackup"
+	mkdir $BACKUP_FOLDER -p
+
+	cp /home/$USER/.bashrc $BACKUP_FOLDER/.bashrc.user-backup
+	cp $PWD/dotfiles/.bashrc-user /home/$USER/.bashrc
+	cp $PWD/dotfiles/snippets /home/$USER/.bash_aliases
+
+	sudo cp /root/.bashrc $BACKUP_FOLDER/.bashrc.root-backup
+	sudo cp $PWD/dotfiles/.bashrc-root /root/.bashrc
+	sudo cp $PWD/dotfiles/snippets /root/.bash_aliases
+
+	sudo cp /etc/skel/.bashrc $BACKUP_FOLDER/.bashrc.skel-backup
+	sudo cp $PWD/dotfiles/.bashrc-user /etc/skel/.bashrc
+	sudo cp $PWD/dotfiles/snippets /etc/skel/.bash_aliases
+
 ## Update the System ##
-	
-	#sudo pacman -Syu -y
-	
+
+	sudo pacman -Syu 		--noconfirm
+
 ## Install Softwares (Pacman) ##
 
-	#sudo pacman -S git --noconfirm
+	sudo pacman -S git 		--noconfirm
+	sudo pacman -S virtualbox 	--noconfirm
+	sudo pacman -S piper 		--noconfirm
+	sudo pacman -S gparted 		--noconfirm
+	sudo pacman -S steam 		--noconfirm
+	sudo pacman -S discord 		--noconfirm
+	sudo pacman -S gnome-tweaks 	--noconfirm
+	sudo pacman -S thunderbird	--noconfirm
+	sudo pacman -S snapd 		--noconfirm
 
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+	export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+	nvm install 16 ; nvm use 16
 
 ## Install Softwares (AUR) ##
-	
+
 	#Google-Chrome
 		git clone https://aur.archlinux.org/google-chrome.git
 		sudo chmod 777 google-chrome ; cd google-chrome ; makepkg -s
 		mv *.deb google_chrome.deb ; mv *.zst google_chrome.zst
-		sudo pacman -U google_chrome.zst --noconfirm
-	
+		sudo pacman -U google_chrome.zst --noconfirm		
+
 ## Install Softwares (Snap) ##
+
+	sudo snap install code
+	sudo snap install spotify
+	sudo snap install telegram-desktop
 
 ## Install Softwares (Flatpak) ##
 
 ## Install Extentions (Gnome Desktop Only) ##
 
-	#sensible-browser \
-		#https://chrome.google.com/webstore/detail/gnome-shell-integration/gphhapmejobijbbhgpjhcjognlahblep \
-		#https://extensions.gnome.org/extension/7/removable-drive-menu/ \
-		#https://extensions.gnome.org/extension/906/sound-output-device-chooser/ \
-		#https://extensions.gnome.org/extension/19/user-themes/
+	xdg-open https://chrome.google.com/webstore/detail/gnome-shell-integration/gphhapmejobijbbhgpjhcjognlahblep
+	xdg-open https://extensions.gnome.org/extension/7/removable-drive-menu/
+	xdg-open https://extensions.gnome.org/extension/906/sound-output-device-chooser/
+	xdg-open https://extensions.gnome.org/extension/19/user-themes/
