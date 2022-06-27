99ad96c0e8e307691bbbaec3191bd2ac8ca6e009
Gabriel N. Silva <106001507+gnsbriellh@users.noreply.github.com>
2022-06-27 03:08:34 -0300

    Update ar.sh

--- ar.sh
+++ ar.sh
@@ -12,23 +12,26 @@
 
 ## Pre-Boot ##
 
-    timedatectl set-ntp true    # To ensure the system clock is accurate
-    timedatectl status  # To check the service status
+    timedatectl set-ntp true # To ensure the system clock is accurate
 
     ## Partitioning ##
 
-        fdisk -l    # To list all available drivers
-        fdisk /dev/nvme0n1  # To Partition a specific driver
+        fdisk -l # To list all available drivers
+        fdisk /dev/nvme0n1 # To Partition a specific driver
 
             # p1= +266290M  | Linux filesystem
             # p2= +807M     | EFI System
             # p3= +16434M   | Linux Swap
             # p4= [ENTER]   | Linux filesystem
+	
+	## Formating ##
 
         mkfs.ext4 /dev/nvme0n1p1    # Format root driver to ext4
         mkfs.fat -F32 /dev/nvme0n1p2    # Format boot driver to fat32
         mkswap /dev/nvme0n1p3   # Format swap driver to linux-swap
         mkfs.ext4 /dev/nvme0n1p4    # Format home driver to ext4
+
+	## Mounting ##
 
         mount /dev/nvme0n1p1 /mnt
         mkdir /mnt/boot/efi -p
@@ -44,40 +47,42 @@
 
     ## Configuring the system ##
 
-        genfstab -U /mnt >> /mnt/etc/fstab                          # Gerenerates the FSTAB file
+        genfstab -U /mnt >> /mnt/etc/fstab # Gerenerates the FSTAB file
 
-        arch-chroot /mnt                                            # Chroot to /mnt
+        arch-chroot /mnt # Chroot to /mnt
 
         ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime # Set Time Zone
-        hwclock --systohc                                           # Generate /etc/adjtime
 
-        nano /etc/locale.gen                                        # Uncomment ¨en_US.UTF-8 UTF-8¨
-        locale-gen                                                  # Generate the locales
-        echo LANG=en_US.UTF-8 >> /etc/locale.conf                   # Set LANG variable
-        pt_BR.UTF-8
+        hwclock --systohc # Generate /etc/adjtime
 
-        echo *HOSTNAME* >> /etc/hostname                            # Set the Hostname
+        nano /etc/locale.gen # Uncomment ¨en_US.UTF-8 UTF-8¨
+        
+        locale-gen # Generate the locales
+        
+        echo LANG=en_US.UTF-8 >> /etc/locale.conf # Set LANG variable
 
-        nano /etc/machine-info                                      # set the Pretty Hostname
+        echo *HOSTNAME* >> /etc/hostname # Set the Hostname
+
+        nano /etc/machine-info # ADD:
             ## PRETTY_HOSTNAME="PRETTY HOSTNAME"
             ## ICON_NAME=computer
             ## CHASSIS=desktop
             ## DEPLOYMENT=production
 
-        nano /etc/hosts                                             # Configure localhosts
+        nano /etc/hosts # ADD:
             ## 127.0.0.1    localhost
             ## ::1          localhost
-            ## 127.0.1.1    *HOSTNAME*
+            ## 127.0.1.1    *HOSTNAME*.localdomain *HOSTNAME*
 
     ## Uncomment multilib to increase package providers ##
 
-        nano /etc/pacman.conf
+        nano /etc/pacman.conf # Uncomment:
         ## [multilib]
         ## Include = /etc/pacman.d/mirrorlist
-        pacman -Sy
 
     ## Installing Other Packages ##
 
+        pacman -Sy
         pacman -S man-db
         pacman -S man-pages
         pacman -S dosfstools
@@ -88,66 +93,78 @@
         pacman -S wpa_supplicant
         pacman -S wireless_tools
         pacman -S dialog
+        pacman -S ufw
         pacman -S grub
         pacman -S efibootmgr
         pacman -S intel-ucode
-
-        systemctl enable NetworkManager.service # permanently enable network
 
     ## Config Boot Loader ##
 
         grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck # Complete installation of Grub and Microcode
 
-        nano /etc/default/grub
+        nano /etc/default/grub # CHANGE:
             # GRUB_TIMEOUT=*0.1*
+            
         grub-mkconfig -o /boot/grub/grub.cfg    # generate grub.cfg
+
+	passwd root # change root's password
+	
+    ## Reboot ##
+
+        exit
+        umount -R /mnt
+        reboot
+
+    ## Install GPU Drivers ##
+
+        pacman -S xorg
+        pacman -S mesa
+        pacman -S lib32-mesa
+        pacman -S xf86-video-amdgpu
+        pacman -S vulkan-radeon
+        pacman -S lib32-vulkan-radeon
+        pacman -S libva-mesa-driver
+        pacman -S lib32-libva-mesa-driver
+        pacman -S mesa-vdpau
+        pacman -S lib32-mesa-vdpau
+    
+    ## Install a  Display Manager##
+
+	pacman -S sddm
+
+    ## Install a Desktop Environment ##
+
+	pacman -S plasma
+	pacman -S plasma-desktop
+	pacman -S plasma-wayland-session
+	pacman -S kdegraphics-thumbnailers
+	pacman -S ffmpegthumbs
+	pacman -S kde-gtk-config
+	pacman -S packagekit-qt5
+	pacman -S alsa-utils
+	#pacman -S kde-applications
+
+    ## Permanently Activate Services ##
+
+	systemctl enable sddm.service
+	systemctl enable NetworkManager.service
+	systemctl enable ufw.service
 
     ## Create a new user and add to sudoers ##
 
-        useradd -m -G wheel -s /bin/bash *user*   # Create User
+        useradd -m -G wheel -s /bin/zsh *user*   # Create User
+        
         passwd *user*   # Change User password
 
         EDITOR=nano visudo # Uncomment the wheel group to add user to sudoers
 
     ## Reboot ##
 
-        exit
-        umount -R /mnt
-        reboot now
-
-    ## Install GPU Drivers ##
-
-        sudo pacman -S xorg
-        sudo pacman -S mesa
-        sudo pacman -S lib32-mesa
-        sudo pacman -S xf86-video-amdgpu
-        sudo pacman -S vulkan-radeon
-        sudo pacman -S lib32-vulkan-radeon
-        sudo pacman -S libva-mesa-driver
-        sudo pacman -S lib32-libva-mesa-driver
-        sudo pacman -S mesa-vdpau
-        sudo pacman -S lib32-mesa-vdpau
-
-    ## Install a Desktop Environment ##
-
-        sudo pacman -S gdm
-
-        sudo pacman -S gnome
-        sudo pacman -S power-profiles-daemon
-        sudo pacman -S gnome-tweaks
-        sudo pacman -S gnome-shell-extensions
-        sudo pacman -S gnome-shell-extension-appindicator
-        sudo pacman -S webp-pixbuf-loader
-        #sudo pacman -S chrome-gnome-shell (AUR)
-        #sudo pacman -S gnome-extra
-
-        sudo systemctl start gdm.service
-        sudo systemctl enable gdm.service
-
+        reboot
 
 ## Post Install ##
 
-        sudo pacman -S lib32-fontconfig ttf-liberation wqy-zenhei ## fix steam fonts
+        sudo pacman -S ttf-dejavu ttf-liberation ## fix steam fonts
         sudo pacman -S noto-fonts-emoji # Fix Emoji Selector showing blank Squares.
-        sudo pacman -S ufw # Installs a Firewall
-        sudo systemctl enable ufw.service
+
+
