9651a50a5c2ccd755113eddb3a70601fa049ed63
gnsbriellh <106001507+gnsbriellh@users.noreply.github.com>
2022-07-01 00:48:02 -0300

    update

---
+++ 1-Install_LiveDisk.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+## System Configuration ##
+
+        # Config Time Zone #
+        ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime -v
+
+        # Cofig Hardware Clock #
+        hwclock --systohc -v
+
+        # Config Locale #
+        sed -i -e 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+        locale-gen
+        echo LANG=en_US.UTF-8 >> /etc/locale.conf
+
+        # Config Hostname #
+        echo gnsbriellh-desktop >> /etc/hostname
+
+        # Config Pretty Hostname #
+        PRETTY_HOSTNAME="Gabriel's Desktop"
+        ICON_NAME="computer"
+        CHASSIS="desktop"
+        DEPLOYMENT="production"
+
+        echo PRETTY_HOSTNAME='"'$PRETTY_HOSTNAME'"' >> /etc/machine-info
+        echo ICON_NAME=$ICON_NAME >> /etc/machine-info
+        echo CHASSIS=$CHASSIS >> /etc/machine-info
+        echo DEPLOYMENT=$DEPLOYMENT >> /etc/machine-info
+
+        # Config Hosts #
+        echo "127.0.0.1    localhost" >> /etc/hosts
+        echo "::1          localhost" >> /etc/hosts
+        echo "127.0.1.1    gnsbriellh-desktop.localdomain gnsbriellh-desktop" >> /etc/hosts
+
+        # Add Multilib to pacman #
+        echo "[multilib]" >> /etc/pacman.conf
+        echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+
+# Necessary Packages #
+
+        pacman -Sy                              --noconfirm
+        pacman -S man-db                        --noconfirm
+        pacman -S man-pages                     --noconfirm
+        pacman -S dosfstools                    --noconfirm
+        pacman -S os-prober                     --noconfirm
+        pacman -S mtools                        --noconfirm
+        pacman -S wpa_supplicant                --noconfirm
+        pacman -S wireless_tools                --noconfirm
+        pacman -S dialog                        --noconfirm
+        pacman -S networkmanager                --noconfirm
+        pacman -S network-manager-applet        --noconfirm
+
+        systemctl enable NetworkManager.service
+
+## Boot Manager ##
+
+        # GRUB #
+        pacman -S grub                          --noconfirm
+        pacman -S efibootmgr                    --noconfirm
+        pacman -S intel-ucode                   --noconfirm
+
+        grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck
+        sed -i -e 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0.1/g' /etc/default/grub
+        grub-mkconfig -o /boot/grub/grub.cfg
+
+## GPU & Video Drivers ##
+
+        # AMD #
+        pacman -S xorg                          --noconfirm
+        pacman -S mesa                          --noconfirm
+        pacman -S lib32-mesa                    --noconfirm
+        pacman -S xf86-video-amdgpu             --noconfirm
+        pacman -S vulkan-radeon                 --noconfirm
+        pacman -S lib32-vulkan-radeon           --noconfirm
+        pacman -S libva-mesa-driver             --noconfirm
+        pacman -S lib32-libva-mesa-driver       --noconfirm
+        pacman -S mesa-vdpau                    --noconfirm
+        pacman -S lib32-mesa-vdpau              --noconfirm
+
+## Display Manager ##
+
+        # KDE-Plasma #
+        pacman -S sddm                          --noconfirm
+        systemctl enable sddm.service
+
+        # Gnome #
+        #pacman -S gdm
+        #systemctl enable gdm.service
+
+## Desktop Environment ##
+
+        # Plasma KDE #
+        pacman -S plasma                        --noconfirm
+        pacman -S plasma-desktop                --noconfirm
+        pacman -S konsole                       --noconfirm
+
+        # Gnome #
+        #pacman -S gnome
+        #pacman -S gnome-terminal
+        #pacman -S gnome-extra
+
+## Create User ##
+
+        SET_USER="gnsbriellh"
+
+        useradd -m -G wheel -s /bin/bash $SET_USER
+
+        echo "Set password for $SET_USER"
+        passwd gnsbriellh
+
+        echo "Set password for root user"
+        passwd root
+
+        sleep 2 ; echo "Visudo will now open in 7s..." ; sleep 7
+        EDITOR=nano visudo
+
+## Done... ##
+
+        echo "Installation Completed !"
--- 3-Install_Terminal.sh
+++ 3-Install_Terminal.sh
@@ -2,18 +2,17 @@
 
 ##  ##
 if [ ! "${EUID:-$(id -u)}" -eq 0 ] ; then
-	#Git
 
+	#Git
 	sudo wget -c -P $PWD/dotfiles/ https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 
 	#Root
-
 	sudo cp $PWD/dotfiles/.bashrc 		/root/.bashrc -v
 	sudo cp $PWD/dotfiles/.bash_aliases /root/.bash_aliases -v
 	sudo mkdir /root/.config/bash/ -p -v
 	sudo cp $PWD/dotfiles/git-prompt.sh /root/.config/bash/git-prompt.sh -v
-	#User
 
+	#User
 	cp $PWD/dotfiles/.bashrc 		~/ -v
 	cp $PWD/dotfiles/.bash_aliases 	~/ -v
 	mkdir ~/.config/bash/ -p -v
@@ -21,8 +20,6 @@ if [ ! "${EUID:-$(id -u)}" -eq 0 ] ; then
 
 else
 
-	echo "One shouldn't be logged as root to perform this operation..."
+	echo "One shouldn't be logged as root to perform this operation. Try again..."
 
 fi
-
-#~/.config/prompt/git-prompt.sh
